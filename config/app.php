645d0b81c4f590421c5d45ce4000f6e20be483ab
senghabcheng <scheng@paragoniu.edu.kh>
2026-09-05 21:29:13 +0700

    restore previous commit

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY', 'base64:nREO3yRnAcSPSCOVognHyvyELrqnPzHwaV96YFjgvxM='),
+    'key' => env('APP_KEY'),
 
     'previous_keys' => [
         ...array_filter(
